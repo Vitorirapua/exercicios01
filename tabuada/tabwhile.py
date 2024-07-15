703d30afae1be8c4f9b3d74cddb12a399d473091
Vitor Irapuã <vitorirapua.souza@gmail.com>
2024-07-15 16:57:39 -0300

    Create tabwhile.py

---
+++ tabuada/tabwhile.py
@@ -0,0 +1,13 @@
+print(" ")
+print("Tabuada de 'UM a 'DEZ' ")
+print("  ")
+print("---------------------------")
+
+x = 1
+while x < 11:
+    y = 1
+    while y < 11:
+        print (x, "X", y, "=", x*y)
+        y += 1
+    print("-----------------------")    
+    x += 1
