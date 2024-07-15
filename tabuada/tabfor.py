19da0251c38a925ef585d1eedda2219932a94f99
Vitor Irapuã <vitorirapua.souza@gmail.com>
2024-07-15 16:56:40 -0300

    Create tabfor.py

---
+++ tabuada/tabfor.py
@@ -0,0 +1,9 @@
+print(" ")
+print("Tabuada de 'UM a 'DEZ' ")
+print("  ")
+print("---------------------------")
+
+for x in range (1, 11):
+    for y in range (11):
+        print (x, "X", y, "=", x*y)
+    print("-----------------------")
