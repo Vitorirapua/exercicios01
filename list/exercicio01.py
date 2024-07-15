3fe0f7a70c2f5084563a07ebebb9724fccd308b5
Vitor Irapuã <vitorirapua.souza@gmail.com>
2024-07-15 16:52:21 -0300

    Create exercicio01.py

---
+++ list/exercicio01.py
@@ -0,0 +1,11 @@
+minha_lista = ["banana", "maçã", "uva", "laranja"]
+
+print(minha_lista[1])
+
+minha_lista.append("kiwi")
+
+print("a quantidade de frutas da lista é: ", (len(minha_lista)))
+
+minha_lista.remove("laranja")
+
+print("Lista após remover a ultima fruta", minha_lista)
